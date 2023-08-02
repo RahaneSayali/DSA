1492c83d8837f9a1d189f5e3cb667c4cf4c4562c
Sayali Rahane <113023490+RahaneSayali@users.noreply.github.com>
2023-08-02 17:36:27 +0530

    Add files via upload

---
+++ Array/Print_Pairs.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+void printAllPairs(int arr[],int n){
+    //print all the elements (2 Nested loop)
+    for(int i=0;i<n;i++){
+        int x=arr[i];
+        for(int j=i+1;j<n;j++){ //j=0
+            int y=arr[j];
+            cout<<x<<" , "<<y<<endl;
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    int arr[]={10,20,30,40,50,60};
+    int n=sizeof(arr)/sizeof(int);
+
+    printAllPairs(arr,n);
+    return 0;
+}
