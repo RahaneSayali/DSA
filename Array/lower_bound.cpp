6bc90e1b550297c32d30902d50f3be8663fab1e9
Sayali Rahane <113023490+RahaneSayali@users.noreply.github.com>
2023-08-10 22:36:44 +0530

    Add files via upload

---
+++ Array/lower_bound.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int lower_bound(vector<int> A, int val){
+
+   int s=0;
+   int e=A.size()-1;
+   int ans=-1;
+
+   while (s<=e){
+    int mid=(s+e)/2;
+    if(A[mid]<=val){
+        ans=A[mid];
+        s=mid +1;
+    }
+    else{
+        e=mid-1;
+    }
+   }
+   return ans;
+}
