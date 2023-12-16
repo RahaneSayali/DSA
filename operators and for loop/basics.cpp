63a8c4aac0d76ad2ff5f65b53855dc70f3d3a2a0
RahaneSayali <sayalipr15@gmail.com>
2023-12-16 13:09:10 +0530

    added operator eg

--- operators and for loop/basics.cpp
+++ operators and for loop/basics.cpp
@@ -9,6 +9,15 @@
 // In NOT invert means o to 1 viceversa
 // In XOR 11 =0 and 01=1,10=1,00=0 means only one time 1 op will be 1
 
+
+// left shift and right shift
+// eg. 5<<1 =101  --- left shift(<<) 
+// here 5 will shift to left 1 time i.e 1010 
+// 5>>1=101  --right shift(>>)
+// here 5 will shift to right 1 time i.e 001 
+
+
+
 #include <iostream>
 using namespace std;
 int main()
@@ -27,15 +36,17 @@ int main()
     cout << (19 << 1) << endl;
     cout << (21 << 2) << endl;
 
+// increment decrement operator
+
     int i = 7;
 
-    cout << (++i) << endl;
+    cout << (++i) << endl;    /* pre increment */
     // 8
-    cout << (i++) << endl;
+      cout << (i++) << endl;  /* post increment */
     // 8 , i=9
-    cout << (i--) << endl;
+    cout << (i--) << endl;    /* post decrement */
     // 9, i=8
-    cout << (--i) << endl;
+    cout << (--i) << endl;    /* pre decrement */
     // 7, i=7
 
 }
--- operators and for loop/mcq.cpp
+++ operators and for loop/mcq.cpp
@@ -12,19 +12,58 @@
 //         //1
 // }
 
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int a = 1;
+//     int b = 2;
+//     if (a-- > 0 && ++b > 2)
+//     {
+//         cout << "Stage - Inside If ";
+//     }
+//     else
+//     {
+//         cout << "Stage2- Inside else";
+//     }
+//     cout << a << " " << b << endl;
+// }
+// Stage - Inside If 0 3
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int a=1;
+//     int b=2;
+//     if(a-->0 || ++b>2){
+//         cout<<"Stage1 - Inside if ";
+//     }
+//     else{
+//         cout<<"Stage2 - Inside else";
+//     }
+//     cout<<a<<" "<<b<<endl;
+// }
+// Stage2 - inside if 0 2
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//  int number =3;
+//  cout<<(25*(++number));
+// }
+// 25*4=100
+
 #include <iostream>
 using namespace std;
 int main()
 {
-    int a = 1;
-    int b = 2;
-    if (a-- > 0 && ++b > 2)
-    {
-        cout << "Stage - Inside If ";
-    }
-    else
-    {
-        cout << "Stage2- Inside else";
-    }
-    cout << a << " " << b << endl;
+    int a=1;
+    int b=a++;
+    
+    int c=++a;
+    cout<<b;
+    cout<<c;
 }
+//1 3
