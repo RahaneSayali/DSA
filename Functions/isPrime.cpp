efeda06d5aba60bcfafa09077a31013dee19f884
RahaneSayali <sayalipr15@gmail.com>
2023-12-20 16:02:34 +0530

    pass by value etc

--- Functions/isPrime.cpp
+++ Functions/isPrime.cpp
@@ -4,10 +4,26 @@ using namespace std;
 // 0 -->not prime no.
 bool isPrime(int n)
 {
-    
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            return 0;
+        }
+    }
+    return 1;
 }
 int main()
 {
-
+    int n;
+    cin >> n;
+    if (isPrime(n))
+    {
+        cout << "num is prime no" << endl;
+    }
+    else
+    {
+        cout << "not a prime no" << endl;
+    }
     return 0;
 }
