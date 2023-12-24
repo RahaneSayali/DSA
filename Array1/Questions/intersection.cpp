9d24e2abac7c33c866954cc52b377ee22bdff373
RahaneSayali <sayalipr15@gmail.com>
2023-12-24 18:00:49 +0530

    hehe

--- Array1/Questions/intersection.cpp
+++ Array1/Questions/intersection.cpp
@@ -1,43 +1,49 @@
-//time too much 
-#include <bits/stdc++.h> 
+// TLE
+#include <bits/stdc++.h>
 vector<int> findArrayIntersection(vector<int> &arr1, int n, vector<int> &arr2, int m)
 {
-	vector<int>ans;
-	for(int i=0;i<n;i++){
-		int element=arr1[i];
+    vector<int> ans;
+    for (int i = 0; i < n; i++)
+    {
+        int element = arr1[i];
 
-		for(int j=0;j<m;j++){
+        for (int j = 0; j < m; j++)
+        {
 
-			if(element==arr2[j]){
-				ans.push_back(element);
-				arr2[j]=-2; //random any value
-				break;
-			}
-		}
-	}
-	return ans;
+            if (element == arr2[j])
+            {
+                ans.push_back(element);
+                arr2[j] = -2; // random any value
+                break;
+            }
+        }
+    }
+    return ans;
 }
 
-//brute solution
-#include <bits/stdc++.h> 
+// brute solution 2 point approched
+#include <bits/stdc++.h>
 vector<int> findArrayIntersection(vector<int> &arr1, int n, vector<int> &arr2, int m)
 {
-	int i=0,j=0;
-	vector<int>ans;
-	while(i<n&&j<m){
-		if(arr1[i]==arr2[j])
-		{
-			ans.push_back(arr1[i]);
-			i++;
-			j++;
-		}
-		else if(arr1[i]<arr2[j]){
-			i++;
-		}
-		else{
-			j++;
-		}
-	}
-	
-	return ans;
+    int i = 0, j = 0;
+    vector<int> ans;
+    while (i < n && j < m)
+    {
+        if (arr1[i] == arr2[j])
+        {
+            ans.push_back(arr1[i]);
+            i++;
+            j++;
+        }
+        else if (arr1[i] < arr2[j])
+        {
+            i++;
+        }
+        else
+        {
+            j++;
+        }
+    }
+
+    return ans;
 }
