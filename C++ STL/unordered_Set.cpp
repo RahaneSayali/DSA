25be73175cfac7912618ef4c8ae1ad51caeca32d
RahaneSayali <sayalipr15@gmail.com>
2024-01-25 13:29:33 +0530

    unordered_set

---
+++ C++ STL/unordered_Set.cpp
@@ -0,0 +1,9 @@
+void explainSet(){
+    unordered_set<int> st;
+    //lower_bound nd upper_bound function
+    //does not works,rest all functions are same
+    //as above,it does not stores in ant
+    //particular order it has a better complexity
+    //than set in most cases,except some when collision happens
+    
+}
