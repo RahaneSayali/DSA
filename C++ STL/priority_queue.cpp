65efb8220678b34364ff4b3e23afe032f0797cb6
RahaneSayali <sayalipr15@gmail.com>
2024-01-25 12:43:25 +0530

    tc

--- C++ STL/priority_queue.cpp
+++ C++ STL/priority_queue.cpp
@@ -1,11 +1,11 @@
 void explainPQ(){
     priority_queue<int>pq;
     pq.push(5); //{5}
-    pq.push(8); //{8,5,2}
+    pq.push(8); //{8,5,2} //log n
     pq.emplace(10); //{10,8,5,2}
 
-    cout<<pq.top(); //prints 10
-    pq.pop(); //{8,5,2}
+    cout<<pq.top(); //prints 10  //o(1)
+    pq.pop(); //{8,5,2}  //o(1)
     cout<<pq.top(); //prints 8
 
     //size swap empty function same as others
