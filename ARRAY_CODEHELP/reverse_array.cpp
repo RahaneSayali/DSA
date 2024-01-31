3b3615d7df710133a7c7438e1ad19ce86b58795c
RahaneSayali <sayalipr15@gmail.com>
2024-01-31 20:55:28 +0530

    direct logic

--- ARRAY_CODEHELP/reverse_array.cpp
+++ ARRAY_CODEHELP/reverse_array.cpp
@@ -30,3 +30,12 @@ int main(){
     cout<<"reverse of array"<<endl;
     print(ans);
 }
+
+/*void reverseArray(vector<int> &arr , int m){
+    int s=m+1,e=arr.size()-1;
+    while(s<=e){
+        swap(arr[s],arr[e]);
+        s++;
+        e--;
+    }    	
+}*/
