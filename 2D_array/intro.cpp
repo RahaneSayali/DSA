e0343a66c25e47127542bb9c37a75678fd50da7c
RahaneSayali <sayalipr15@gmail.com>
2024-02-18 14:27:16 +0530

    2d basic

--- 2D_array/intro.cpp
+++ 2D_array/intro.cpp
@@ -20,9 +20,9 @@ int main(){
     }
 
 //print
-for(int i=0;i<3;i++){
-   for(int j=0;j<4;j++){
-    cout<<arr[i][j]<<" ";
+for(int row=0;row<3;row++){
+   for(int col=0;col<4;col++){
+    cout<<arr[row][col]<<" ";
    }
    cout<<endl;
 }
