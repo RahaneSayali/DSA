feed98f0403516b600ac015e02964e30e0fd581d
RahaneSayali <sayalipr15@gmail.com>
2024-02-18 14:26:05 +0530

    ..

--- 2D_array/intro.cpp
+++ 2D_array/intro.cpp
@@ -13,9 +13,9 @@ int main(){
  */
 
   //taking input->row wise input
-    for(int i=0;i<4;i++){
-        for(int j=0;j<3;j++){
-        cin>>arr[j][i];
+    for(int col=0;col<4;col++){
+        for(int row=0;row<3;row++){
+        cin>>arr[row][col];
     }
     }
 
