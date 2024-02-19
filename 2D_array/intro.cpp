a0dae59e920c98f794741315096350ef32a390c2
RahaneSayali <sayalipr15@gmail.com>
2024-02-19 20:48:20 +0530

    largest row prob

--- 2D_array/intro.cpp
+++ 2D_array/intro.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespace std;
-bool isPresent(int arr[][4], int target, int row, int col)
+bool isPresent(int arr[][3], int target, int row, int col)
 {
-    for (int col = 0; col < 4; col++)
+    for (int col = 0; col < 3; col++)
     {
         for (int row = 0; row < 3; row++)
         {
@@ -14,11 +14,25 @@ bool isPresent(int arr[][4], int target, int row, int col)
     }
     return 0;
 }
+// to print row wise sum
+void printSum(int arr[][3], int row, int col)
+{
+    cout << "printing Sum ->" << endl;
+    for (int row = 0; row < 3; row++)
+    {
+        int sum = 0;
+        for (int col = 0; col < 3; col++)
+        {
+            sum += arr[row][col];
+        }
+        cout << sum << " ";
+    }
+}
 
 int main()
 {
     // create 2d array
-    int arr[3][4];
+    int arr[3][3];
     // int arr[3][4]={1,2,3,4,5,6,7,8,9,10,11,12};
     // int arr[3][4]={{1,11,111,1111},{2,22,222,2222},{3,33,333,3333}};
 
@@ -29,9 +43,9 @@ int main()
     }
  }
  */
-
+    cout << "enter the elements" << endl;
     // taking input->col wise input
-    for (int col = 0; col < 4; col++)
+    for (int col = 0; col < 3; col++)
     {
         for (int row = 0; row < 3; row++)
         {
@@ -42,21 +56,26 @@ int main()
     // print
     for (int row = 0; row < 3; row++)
     {
-        for (int col = 0; col < 4; col++)
+        for (int col = 0; col < 3; col++)
         {
             cout << arr[row][col] << " ";
         }
         cout << endl;
     }
-    cout<<"Enter the element to search"<<endl;
+    cout << "Enter the element to search" << endl;
     int target;
-    cin>>target;
+    cin >> target;
 
-    if(isPresent(arr,target,3,4)){
-        cout<<"Elment found"<<endl;
+    if (isPresent(arr, target, 3, 4))
+    {
+        cout << "Elment found" << endl;
     }
-    else{
-        cout<<"Not found"<<endl;
+    else
+    {
+        cout << "Not found" << endl;
     }
+    printSum(arr, 3, 3);
+
+    
     return 0;
 }
