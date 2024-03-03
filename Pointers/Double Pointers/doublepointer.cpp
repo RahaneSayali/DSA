580e73205bac29f0db4ae1417bd393793bb69d03
RahaneSayali <sayalipr15@gmail.com>
2024-03-03 13:10:47 +0530

    double pointers

---
+++ Pointers/Double Pointers/doublepointer.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+
+void update(int **p2){
+     //p2=p2+1;  //->no change
+     
+     //*p2=*p2+1;  //->change
+
+      //**p2=**p2+1;  //no change
+
+}
+
+void update(int *p){
+        *p = (*p)  *  2;
+    }
+        void increment(int **p){
+        ++(**p);
+    }
+
+int main(){
+    int i=5;
+    int *p=&i;
+    int ** p2=&p;
+
+  /*  cout<<"all good"<<endl;
+     
+    cout<<"printing p"<<p<<endl;
+    //cout<<"address of p"<<&p<<endl;
+
+    cout<<*p2<<endl;
+    
+    cout<<i<<endl;
+    cout<<*p<<endl;
+    cout<<**p2<<endl;
+
+    cout<<&i<<endl;
+    cout<<p<<endl;
+    cout<<*p2<<endl;
+
+    cout<<&p<<endl;
+    cout<<p2<<endl;
+
+ 
+  cout<<endl<<endl;
+  cout<<"before"<<i<<endl;
+  cout<<"before"<<p<<endl;
+  cout<<"before"<<p2<<endl;
+
+  update(p2);
+  cout<<"after"<<i<<endl;
+  cout<<"after"<<p<<endl;
+  cout<<"after"<<p2<<endl;
+  cout<<endl<<endl;
+*/
+  
+        int num = 110;
+        int *ptr = &num;
+        increment(&ptr);
+        cout << num << endl;
+
+    return 0;
+}
