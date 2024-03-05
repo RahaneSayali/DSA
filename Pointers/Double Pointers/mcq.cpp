6663ebc1f755bc5f99929234077a8815ebfdb491
RahaneSayali <sayalipr15@gmail.com>
2024-03-05 21:39:46 +0530

    pointers mcq

---
+++ Pointers/Double Pointers/mcq.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    /*
+        int first = 8;
+        int second = 18;
+        int *ptr = &second;
+        *ptr = 9;
+        cout  << first << " " << second << endl; //8 9
+
+
+        int first = 6;
+        int *p = &first;
+        int *q = p;
+        (*q)++;
+        cout << first  << endl;
+
+    int first = 8;
+    int *p = &first;
+    cout << (*p)++ << " ";  //post increment
+    cout << first << endl;
+
+ int *p = 0;  //null pointer p=&i
+    int first = 110;
+    *p = first;  //p= &first
+    cout << *p << endl;
+
+
+    int first = 8;
+    int second = 11;
+    int *third = &second;
+    first = *third;
+    *third = *third + 2;
+    cout << first << "  " << second << endl;
+*/
+
+    return 0;
+}
