f283fc5f0212464b1fdb6593ada0890fc1de4aa5
RahaneSayali <sayalipr15@gmail.com>
2024-03-06 22:42:52 +0530

    referene error

--- Dynamic Allocation/reference_variable.cpp
+++ Dynamic Allocation/reference_variable.cpp
@@ -1,7 +1,26 @@
 #include<iostream>
 using namespace std;
+
+int& func(int a){
+    int num=a;
+    int& ans=num;
+    return ans;
+}
+int* fun(int n){
+    int* ptr =&n;
+    return ptr;
+}
+
+void update2(int& n){
+    n++;
+}
+
+void update1(int n){
+    n++;
+}
+
 int main(){
-    int i=5;
+  /*  int i=5;
     //create refernce variable 
     int &j=i;
     cout<<i<<endl;
@@ -10,6 +29,14 @@ int main(){
     j++;
     cout<<j<<endl;
     cout<<i<<endl;
-    
+*/
+
+    int n=5;
+    cout<<"Before"<<n<<endl;
+    update2(n);
+    cout<<"After"<<n<<endl;
+
+    fun(n);
+
     return 0;
 }
