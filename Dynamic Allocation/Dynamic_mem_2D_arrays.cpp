27db30ad913aa62815d8b30af25370250de2786f
RahaneSayali <sayalipr15@gmail.com>
2024-03-09 14:21:55 +0530

    deletion of memory

--- Dynamic Allocation/Dynamic_mem_2D_arrays.cpp
+++ Dynamic Allocation/Dynamic_mem_2D_arrays.cpp
@@ -1,33 +1,53 @@
 #include <iostream>
 using namespace std;
 int main()
-{
-    int n;
-    cin >> n;
-    int **arr = new int *[n];
 
-    for (int i = 0; i < n; i++)
+{
+    int row;
+    cin >> row;
+
+    int col;
+    cin >> col;
+
+    // int n;
+    // cin >> n;
+
+    // creating 2d array
+    int **arr = new int *[row];
+    for (int i = 0; i < row; i++)
     {
-        arr[i] = new int[n];
+        arr[i] = new int[col];
     }
 
     // creation done
 
     // taking input
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < row; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < col; j++)
         {
             cin >> arr[i][j];
         }
     }
     // taking output
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < row; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < col; j++)
         {
-            cout<< arr[i][j]<<" ";
-        }cout<<endl;
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
     }
+
+    // releasing memory
+    for (int i = 0; i < row; i++)
+    {
+        delete[] arr[i];
+    }
+    delete[] arr;
+
+    // how to create 2d array dynamically
+    // input/output
+    // how to delete memory
     return 0;
 }
