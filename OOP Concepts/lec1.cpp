addb55b6c145501591a9be87c6f983190b8af99f
RahaneSayali <sayalipr15@gmail.com>
2024-04-27 20:12:25 +0530

    static dynamic

--- OOP Concepts/lec1.cpp
+++ OOP Concepts/lec1.cpp
@@ -34,14 +34,34 @@ public:
     }
 };
 int main()
-{
-    // creation of object
-    Hero sayali;
-    cout << "sayali health " << sayali.gethealth() << endl; // will give garbage value
-    // sayali.health = 100; // not accessible as its on private
-    sayali.sethealth(70);
-    sayali.level = 'A';
+{     
+    //static allocation
+    Hero a;
+    a.sethealth(80);
+    a.setlevel('B');
+    cout << "level is  " << a.level << endl;
+    cout << " health is " << a.gethealth() << endl;
 
-    cout << "health is " << sayali.gethealth() << endl;
-    cout << "level is " << sayali.level << endl;
+    //dynamicallly
+    Hero *b = new Hero;
+    b->setlevel('A');
+    b->sethealth(70);
+    cout << "level is  " << (*b).level << endl;
+    cout << " health is " << (*b).gethealth() << endl;
+
+
+    cout << "level is  " << b->level << endl;
+    cout << " health is " << b->gethealth() << endl;
+    
+
+    // // creation of object
+    // Hero sayali;
+    // cout<<"size of sayali is "<<sizeof(sayali)<<endl;
+    // cout << "sayali health " << sayali.gethealth() << endl; // will give garbage value
+    // // sayali.health = 100; // not accessible as its on private
+    // sayali.sethealth(70);
+    // sayali.level = 'A';
+
+    // cout << "health is " << sayali.gethealth() << endl;
+    // cout << "level is " << sayali.level << endl;
 }
